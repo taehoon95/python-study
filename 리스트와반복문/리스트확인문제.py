c11c14c17b25898f353d8fabe42eae98cb84ff5e
taehoon95 <leeth950304@gmail.com>
2021-05-18 17:32:14 +0900

    리스트 확인문제 완료

--- 리스트와반복문/리스트확인문제.py
+++ 리스트와반복문/리스트확인문제.py
@@ -49,6 +49,7 @@ print("5번")
 numbers = [1,2,3,4,5,6,7,8,9]
 output = [[],[],[]]
 
-#for number in numbers:
-    #output[].append(number)
+for number in numbers:
+    output[(number+2) % 3].append(number)
+
 print(output)
