5e23f60e8a3c745d517653dd3e6842eb1e028486
taehoon95 <leeth950304@gmail.com>
2021-05-25 15:18:13 +0900

    튜플

---
+++ 튜플,람다/튜플3.py
@@ -0,0 +1,6 @@
+a, b = 10, 20
+
+print('a : ',a ,'b : ',b, end='\n\n')
+
+a, b = b, a
+print('a : ',a ,'b : ',b, end='\n\n')
---
+++ 튜플,람다/튜플5(enumerate).py
@@ -0,0 +1,16 @@
+list_a = ['a','b','c']
+
+for e in list_a:
+    print(e)
+
+#index : i, value : v 가 들어간다
+for i, v in enumerate(list_a):
+    print(i, v)
+
+a,b = 97,40
+
+x, y= divmod(a,b)
+print('divmod(x,y) : ',x,y,end='\n')
+
+
+
